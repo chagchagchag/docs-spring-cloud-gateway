b7775ed4d25564911d6ecd60f65c87ee74f5fb8d
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-25 13:39:20 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Spring Cloud Gateway</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-spring-cloud-gateway',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-spring-cloud-gateway',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
